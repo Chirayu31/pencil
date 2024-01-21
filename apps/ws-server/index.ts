ca05df49216c8d9a279c4ef884f4b1dd17cf4641
Chirayu31 <chirayupancholi17@gmail.com>
2024-01-21 16:52:39 +0530

    storing username in-memory in server

--- apps/ws-server/index.ts
+++ apps/ws-server/index.ts
@@ -1,8 +1,7 @@
 import express from 'express';
 import http from 'http';
 import { Server, Socket } from 'socket.io';
-
-type Room = string;
+import { Room } from '@repo/types';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -21,27 +20,28 @@ app.get('/', (req, res) => res.send('Hello world'));
 io.on('connection', (socket: Socket) => {
     console.log('A user connected', socket.id);
 
-    socket.on('createRoom', (username) => {
+    socket.on('createRoom', (username: string) => {
         let roomId: string = generateRandomRoomId();
-        while (rooms.includes(roomId)) {
+        const prevRooms = rooms.map(room => room.id)
+
+        while (prevRooms.includes(roomId)) {
             roomId = generateRandomRoomId();
         }
 
-        rooms.push(roomId);
+        rooms.push({ id: roomId, users: [username] });
         socket.join(roomId);
-
-        console.log(socket.id, roomId)
 
         io.to(socket.id).emit('created', roomId);
     });
 
 
-    socket.on('joinRoom', (roomId, username) => {
-        const room = rooms.find((room) => room === roomId);
+    socket.on('joinRoom', (roomId: string, username: string) => {
+        const currRooms = rooms.map(room => room.id)
+        const roomIdx = currRooms.findIndex((room) => room === roomId);
 
-        if (room) {
+        if (roomIdx != -1) {
             socket.join(roomId);
-            console.log(socket.id, roomId)
+            rooms[roomIdx]?.users.push(username)
             io.to(socket.id).emit('joined', roomId);
         } else {
             io.to(socket.id).emit('joined', '');
